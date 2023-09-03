207684c9185b539b78dde154410767a2aa6f480b
Tangerie <josh.cavill01@gmail.com>
2023-09-03 14:57:29 +1000

    new workbox config

--- frontend/workbox-config.js
+++ frontend/workbox-config.js
@@ -1,9 +1,14 @@
 module.exports = {
     globDirectory: "dist",
     globPatterns: [
-        "**/*.{html,js,css,png,svg,jpg,gif,json,woff,woff2,eot,ico,webmanifest,map}"
+        "**/*.{html,js,css,png,svg,jpg,gif,json,woff,woff2,eot,ico,webmanifest}"
     ],
     swDest: "dist/service-worker.js",
     clientsClaim: true,
-    skipWaiting: true
+    skipWaiting: true,
+    modifyURLPrefix: {
+        '': '/dexter/'
+    },
+    cacheId: "Dexter",
+    navigateFallback: "/dexter/"
 };
